799e63090a1250eca91dabebd5f99a9eaf43e0d5
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-10-20 00:08:05 +0300

    [Lesson 2] Solution of "Reverse or rotate" Codewars task
    This is squashed merge commit from following commits:
    
    commit eda1bd56e3bfcdd7b73999d9180474ed011ebaa5
    Author: Tatsiana Sapun <tatiana.sapun@gmail.com>
    Date:   Thu Oct 20 00:00:28 2022 +0300
    
        refactor: move code to separate function
    
    commit 4902409125a24f9fae31253158a3f22b3534ed8e
    Author: Tatsiana Sapun <tatiana.sapun@gmail.com>
    Date:   Wed Oct 19 23:48:56 2022 +0300
    
        feat: implement launch code
    
    commit aef85dd6fbdec0ba330ae5ecbd6364e68cd8ef0d
    Author: Tatsiana Sapun <tatiana.sapun@gmail.com>
    Date:   Wed Oct 19 22:35:55 2022 +0300
    
        feat: add solution
    
    commit 8f9ce44b5e537e0a6634f9cbd822c65daf2e202c
    Author: Tatsiana Sapun <tatiana.sapun@gmail.com>
    Date:   Wed Oct 19 22:17:35 2022 +0300
    
        feat: add custom scripts
    
    commit 945bb04372708049691dd4505607a7df222bd73d
    Author: Tatsiana Sapun <tatiana.sapun@gmail.com>
    Date:   Wed Oct 19 22:16:50 2022 +0300
    
        feat: setup eslint
    
    commit 32040b3a65f4edb8304b39d0a5ec07c3245bb4aa
    Author: Tatsiana Sapun <tatiana.sapun@gmail.com>
    Date:   Wed Oct 19 22:10:36 2022 +0300
    
        feat: setup audit-ci
    
    commit 2f320c077fd81330fe3bf5fb6a6a921b444f2984
    Author: Tatsiana Sapun <tatiana.sapun@gmail.com>
    Date:   Wed Oct 19 22:09:19 2022 +0300
    
        feat: setup typescript
    
    commit f67df50b87b1101740e5878c627d6d3143ff0fef
    Author: Tatsiana Sapun <tatiana.sapun@gmail.com>
    Date:   Wed Oct 19 22:05:45 2022 +0300
    
        init: start task

---
+++ lesson-2/reverse-or-rotate/src/constants/index.ts
@@ -0,0 +1,7 @@
+export const NO_CONTENT = '';
+export const SEPARATOR = '';
+
+export const MESSAGES = {
+  wrongSizeArgType: 'Argument "size" must be a natural number',
+  wrongStringOfDigitsArgType: 'Check argument "string". It must be a string of digits only',
+};
---
+++ lesson-2/reverse-or-rotate/src/solution.ts
@@ -0,0 +1,26 @@
+import { NO_CONTENT, SEPARATOR } from './constants/index.js';
+import { Numbers } from './types/enums.js';
+
+export function revrot(input: string, size: number): string {
+  if (size === Numbers.Zero || input.length < size) {
+    return NO_CONTENT;
+  }
+  const arrayOfChars: string[] = input.split(SEPARATOR);
+  const chunk: string[] = arrayOfChars.splice(Numbers.Zero, size);
+  const sumOfCubes: number = chunk
+    .map(Number)
+    .reduce((sum: number, next: number): number => sum + Math.pow(next, Numbers.Three), Numbers.Zero);
+
+  let result = NO_CONTENT;
+  if (sumOfCubes % Numbers.Two) {
+    result += chunk.splice(Numbers.One).join(SEPARATOR) + chunk[Numbers.Zero];
+  } else {
+    result += chunk.reverse().join(SEPARATOR);
+  }
+
+  if (arrayOfChars.length > size) {
+    result += revrot(arrayOfChars.join(SEPARATOR), size);
+  }
+
+  return result;
+}
